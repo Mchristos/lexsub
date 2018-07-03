1670d5072e590c51c9a827a422ce656db92b842e
chris <chrismarais@protonmail.com>
2018-07-03 15:57:05 +0100

    example usage

---
+++ example_use.py
@@ -0,0 +1,14 @@
+# download Google News word2vec binary file from https://drive.google.com/file/d/0B7XkCwpI5KDYNlNUTTlSS21pQmM/edit?usp=sharing
+
+from lexsub import LexSub
+from gensim.models import KeyedVectors
+
+word2vec_path = "~/GoogleNews-vectors-negative300.bin"
+vectors = KeyedVectors.load_word2vec_format(word2vec_path, binary=True)
+ls = LexSub(vectors, candidate_generator='lin')
+
+sentence = "She had a drink at the pub"
+target = "pub.n"
+result = ls.lex_sub(target, sentence)
+print(result)
+# ['tavern', 'bar', 'cafe', 'restaurant', 'nightclub']
